10d72157297ab4fae431ebe22297e83135567393
Dylan Foster <turtlecannon@gmail.com>
2015-09-15 22:35:40 -0400

    Fixed kij.

--- basic/dgemm_kij.c
+++ basic/dgemm_kij.c
@@ -7,11 +7,11 @@ void square_dgemm(const int M, const double *A, const double *B, double *C)
   int i, j, k;
   for (k = 0; k < M; ++k)
     {
-      for (i = 0; j < M; ++j) 
+      for (i = 0; i < M; ++i) 
 	{
 	  /* double bkj = AT(B,M,k,j); */
 	  double aik = AT(A,M,i,k);
-	  for (j = 0; i < M; ++i) 
+	  for (j = 0; j < M; ++j) 
 	    {
 	      /* double cij = AT(C, M, i, j); */
 	      AT(C, M, i, j) += aik * AT(B,M,k,j);
